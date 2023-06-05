1a2cdf3966c82a30d7c0177ad67eb58fc0049112
taekto <taekputer@gmail.com>
2023-06-05 20:28:10 +0900

    initialize

---
+++ back/src/test/java/com/ssafy/closet/ClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.closet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
